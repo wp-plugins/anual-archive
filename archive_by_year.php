1a2e5b71723b881240638573ede618a17fbdab51
baden03 <baden03@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-19 19:03:11 +0000

    added shortcode and extended options to include all archive types
    git-svn-id: https://plugins.svn.wordpress.org/anual-archive/trunk@334743 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- archive_by_year.php
+++ archive_by_year.php
@@ -3,7 +3,7 @@
 Plugin Name: Annual Archive
 Plugin URI: http://www.twinpictures.de/anual-archive-widget
 Description: Like the default Archive Widget, but grouped by year.
-Version: 1.0
+Version: 1.1
 Author: Twinpictures
 Author URI: http://www.twinpictures.de
 License: GPL2
@@ -37,24 +37,35 @@ class AnualArchives extends WP_Widget {
 		extract($args);
 		$c = $instance['count'] ? '1' : '0';
 		$d = $instance['dropdown'] ? '1' : '0';
-		$y = $instance['yearly'] ? 'monthly' : 'yearly';
+		$type = empty($instance['type']) ? 'yearly' : apply_filters('widget_type', $instance['type']);
+		$limit = apply_filters('widget_limit', $instance['limit']);
 		$title = apply_filters('widget_title', empty($instance['title']) ? __('Annual Archive') : $instance['title'], $instance, $this->id_base);
 
 		echo $before_widget;
 		if ( $title )
 			echo $before_title . $title . $after_title;
 
-		if ( $d ) {
-			$dtitle = 'Select Year';
-			if ( $y == 'monthly')
-				$dtitle = 'Select Month';
+		if ($d) {
+			$dtitle = __('Select Year');
+			if ($type == 'monthly'){
+				$dtitle = __('Select Month');
+			}
+			else if($type == 'weekly'){
+				$dtitle = __('Select Week');
+			}
+			else if($type == 'daily'){
+				$dtitle = __('Select Day');
+			}
+			else if($type == 'postbypost' || $type == 'alpha'){
+				$dtitle = __('Select Post');
+			}
 		?>
-		<select name="archive-dropdown" onchange='document.location.href=this.options[this.selectedIndex].value;'> <option value=""><?php echo esc_attr(__($dtitle)); ?></option> <?php wp_get_archives(apply_filters('widget_anual_archive_dropdown_args', array('type' => $y, 'format' => 'option', 'show_post_count' => $c))); ?> </select>
+		<select name="archive-dropdown" onchange='document.location.href=this.options[this.selectedIndex].value;'> <option value=""><?php echo esc_attr(__($dtitle)); ?></option> <?php wp_get_archives(apply_filters('widget_archive_dropdown_args', array('type' => $type, 'format' => 'option', 'show_post_count' => $c, 'limit' => $limit))); ?> </select>
 		<?php
 		} else {
 		?>
 		<ul>
-		<?php wp_get_archives(apply_filters('widget_anual_archive_args', array('type' => $y, 'show_post_count' => $c))); ?>
+		<?php wp_get_archives(apply_filters('widget_archive_args', array('type' => $type, 'limit' => $limit, 'show_post_count' => $c))); ?>
 		</ul>
 		<?php
 		}
@@ -68,7 +79,8 @@ class AnualArchives extends WP_Widget {
 		$instance['title'] = strip_tags($new_instance['title']);
 		$instance['count'] = $new_instance['count'] ? 1 : 0;
 		$instance['dropdown'] = $new_instance['dropdown'] ? 1 : 0;
-		$instance['yearly'] = $new_instance['yearly'] ? 1 : 0;
+		$instance['type'] = strip_tags($new_instance['type']);
+		$instance['limit'] = strip_tags($new_instance['limit']);
 		return $instance;
 	}
 
@@ -77,20 +89,77 @@ class AnualArchives extends WP_Widget {
 		$title = strip_tags($instance['title']);
 		$count = $instance['count'] ? 'checked="checked"' : '';
 		$dropdown = $instance['dropdown'] ? 'checked="checked"' : '';
-		$yearly= $instance['yearly'] ? 'checked="checked"' : '';
+		$type = strip_tags($instance['type']);
+		$limit = strip_tags($instance['limit']);
 		?>
 		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
 		<p>
 			<input class="checkbox" type="checkbox" <?php echo $count; ?> id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>" /> <label for="<?php echo $this->get_field_id('count'); ?>"><?php _e('Show post counts'); ?></label>
 			<br />
 			<input class="checkbox" type="checkbox" <?php echo $dropdown; ?> id="<?php echo $this->get_field_id('dropdown'); ?>" name="<?php echo $this->get_field_name('dropdown'); ?>" /> <label for="<?php echo $this->get_field_id('dropdown'); ?>"><?php _e('Display as a drop down'); ?></label>
-			<br />
-			<input class="checkbox" type="checkbox" <?php echo $yearly; ?> id="<?php echo $this->get_field_id('yearly'); ?>" name="<?php echo $this->get_field_name('yearly'); ?>" /> <label for="<?php echo $this->get_field_id('yearly'); ?>"><?php _e('Display as a monthly archive'); ?></label>
+		<p>
+			<label for="<?php echo $this->get_field_id('type'); ?>"><?php _e('Archive type:'); ?></label> <select name="<?php echo $this->get_field_name('type'); ?>" id="<?php echo $this->get_field_id('type'); ?>">
+			<?php
+			$types_arr = array(
+				'daily' => 'Daily',
+				'weekly' => 'Weekly',
+				'monthly' => 'Monthly',
+				'yearly' => 'Yearly',
+				'postbypost' => 'Post By Post',
+				'alpha' => 'Alpha'
+			);
+			foreach($types_arr as $key => $value){
+				$selected = '';
+				if($key == $type || (!$type && $key == 'yearly')){
+					$selected = 'SELECTED';
+				}
+				echo '<option value="'.$key.'" '.$selected.'>'.__($value).'</option>';
+			}
+			?>
+			</select>
 		</p>
+		<p><label for="<?php echo $this->get_field_id('limit'); ?>"><?php _e('Limit results to:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('limit'); ?>" name="<?php echo $this->get_field_name('limit'); ?>" type="text" value="<?php echo esc_attr($limit); ?>" /></p>
 		<?php
 	}
 }
 
 // register Archive By Year widget
 add_action('widgets_init', create_function('', 'return register_widget("AnualArchives");'));
+
+// the shortcode
+function annual_archive($atts, $content=null) {
+	extract(shortcode_atts(array(
+		'type' => 'yearly',
+		'limit' => '',
+		'format' => 'html', //html, option
+		'showcount' => '0',
+		'tag' => 'ul',
+	), $atts));
+	
+	if ($format == 'option') {
+		$dtitle = __('Select Year');
+		if ($type == 'monthly'){
+			$dtitle = __('Select Month');
+		}
+		else if($type == 'weekly'){
+			$dtitle = __('Select Week');
+		}
+		else if($type == 'daily'){
+			$dtitle = __('Select Day');
+		}
+		else if($type == 'postbypost' || $type == 'alpha'){
+			$dtitle = __('Select Post');
+		}
+		$arc = '<select name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;"> <option value="">'.esc_attr(__($dtitle)).'</option>';
+		$arc .= wp_get_archives(array('type' => $type, 'limit' => $limit, 'format' => 'option', 'show_post_count' => $showcount, 'echo' => 0)).'</select>';
+	} else if ($format == 'html') {
+		$arc = '<'.$tag.'>';
+		$arc .= wp_get_archives(array('type' => $type, 'limit' => $limit, 'show_post_count' => $showcount, 'echo' => 0));
+		$arc .= '</'.$tag.'>';
+	}
+	return $arc;
+}
+
+add_shortcode('archives', 'annual_archive');
+	
 ?>
--- readme.txt
+++ readme.txt
@@ -5,13 +5,13 @@ Donate link: http://www.twinpictures.de/anual-archive-widget/
 Tags: archive, archives, yearly, monthly, anual, annual, annually, archive by month, archive by year, yearly archive
 Requires at least: 2.8
 Tested up to: 3.1
-Stable tag: 1.0
+Stable tag: 1.1
 
-Annual Archive widget extends default WordPress Archive widget by allowing either monthly or annually archives to be displayed.
+Annual Archive widget extends default WordPress Archive widget by allowing all types of archives to be displayed.
 
 == Description ==
 
-Annual Archive widget extends default WordPress Archive widget by allowing either monthly or annually archives to be displayed.  The Annual Archive widget - It displays archives by year.  Yup, that's pretty much it.
+Annual Archive widget extends default WordPress Archive widget by allowing daily, weekly, monthly, yearly, postbypost and alpha archives to be displayed in the sidebar and in a post or page by using a shortcode. The Annual Archive widget - A better archive.  Yup, that's pretty much it.
 
 == Installation ==
 
@@ -23,8 +23,17 @@ Annual Archive widget extends default WordPress Archive widget by allowing eithe
 
 == Frequently Asked Questions ==
 
+= What are the shortcode options? =
+A <a href='http://www.twinpictures.de/anual-archive-widget/'>complete list of shortcode options</a> has been provided.
+
 = Where might one leave feedback, bug-reports and request features? =
-Why, <a href='http://www.twinpictures.de/anual-archive-widget/'>Annual Archive Page</a> at Twinpictures, of course.
+The <a href='http://www.twinpictures.de/anual-archive-widget/'>Annual Archive Page</a> at Twinpictures page is one popular choice.
+
+= I am a Social Netwookiee, do you have a Facebook page? =
+Yes, yes... <a href='http://www.facebook.com/twinpictures'>Twinpictures is on Facebook</a>.
+
+= Does Twinpictures do the Twiter? =
+Ah yes! <a href='twitter.com/switzerbaden'>@SwitzerBaden at Twitter</a> does the tweeting around here.
 
 = You know annual is not spelled 'anual' in English, right? =
 We do now, so yes.  And thank you for noticing.
@@ -34,14 +43,20 @@ Scares the crap out of the seeing-eye-dogs.
 
 == Screenshots ==
 
-1. Not much to see here... just an extra option that makes this plugin function exactly like the default WordPress Plugin.
+1. The widget options.  Pretty much self-explainatory.
 
 == Changelog ==
+
+= 1.1 =
+* Added additional types and the shortcode.
 
 = 1.0 =
 * The plugin came to be.
 
 == Upgrade Notice ==
+
+= 1.1 =
+Shortcode added and options expanded to include all archive types.
 
 = 1.0 =
 Where once there be not, now there be.
